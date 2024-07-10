4421a26b12ea9149cc5fea0b02608ad9926c860e
aneta <65393573+ANETA-LAURENT@users.noreply.github.com>
2024-07-10 22:16:04 +0200

    Update moon.js

--- moon.js
+++ moon.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
+    const currentDateElement = document.getElementById("current-date");
     const moonPhaseElement = document.getElementById("moon-phase");
     const moonImageElement = document.getElementById("moon-image");
-    const NASA_API_KEY = 'YOUR_NASA_API_KEY';
-    const NASA_API_URL = `https://api.nasa.gov/planetary/apod?api_key=${SXceqBQtBcJO1CF9BCQ2HfutF7kdQMlRTBKCAb61}`;
+    const NASA_API_KEY = 'SXceqBQtBcJO1CF9BCQ2HfutF7kdQMlRTBKCAb61';
+    const NASA_API_URL = `https://api.nasa.gov/planetary/apod?api_key=${NASA_API_KEY}`;
+
+    function getCurrentDate() {
+        const now = new Date();
+        return now.toLocaleDateString('fr-FR', {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric'
+        });
+    }
 
     async function fetchMoonPhase() {
         const response = await fetch(NASA_API_URL);
@@ -24,5 +34,11 @@ document.addEventListener("DOMContentLoaded", function() {
         }
     }
 
+    function updateCurrentDate() {
+        const currentDate = getCurrentDate();
+        currentDateElement.textContent = `Current Date: ${currentDate}`;
+    }
+
+    updateCurrentDate();
     updateMoonPhase();
 });
